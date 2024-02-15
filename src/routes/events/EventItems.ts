a9319ad4173aea74d274dfe003d1be29ab1a1e8c
박찬진 <68864422+developerChans@users.noreply.github.com>
2024-02-15 15:14:06 +0900

    TEST EventItems 개수

--- src/routes/events/EventItems.ts
+++ src/routes/events/EventItems.ts
@@ -2,8 +2,6 @@ import Event01 from "assets/images/events/event_1.png";
 import Event02 from "assets/images/events/event_2.png";
 import Event03 from "assets/images/events/event_3.png";
 import Event04 from "assets/images/events/event_4.png";
-import Event05 from "assets/images/events/event_5.png";
-import Event06 from "assets/images/events/event_6.png";
 
 export const EventItems = [
    {
@@ -33,19 +31,5 @@ export const EventItems = [
       title: "인스타 스토리 이벤트",
       description: "인스타그램 스토리 필터 이벤트",
       link: "https://www.instagram.com/p/CsJToefrzw5/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==",
-   },
-   {
-      id: 5,
-      imageSrc: Event05,
-      title: "스탬프 투어 이벤트",
-      description: "스탬프 채우고 상품 받아가세요!",
-      link: "https://www.instagram.com/p/CsJUEysLfXT/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==",
-   },
-   {
-      id: 6,
-      imageSrc: Event06,
-      title: "보물찾기 이벤트",
-      description: "폭포공원에 숨겨진 보물을 찾자!",
-      link: "https://www.instagram.com/p/CsJVoIQLSeD/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==",
    },
 ];
